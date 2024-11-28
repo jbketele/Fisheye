d0c0f8cfe6717bebbf08c5172703e695bf6cfe3d
Jb Ketele <149502977+jbketele@users.noreply.github.com>
2024-11-28 16:31:50 +0100

    import data et modif template slogan lieu et prix

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,16 +1,25 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        const article = document.createElement('article');
+        const img = document.createElement('img');
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const h2 = document.createElement('h2');
         h2.textContent = name;
+        const p1 = document.createElement('p');
+        p1.textContent = city + " , " + country;
+        const p2 = document.createElement('p');
+        p2.textContent = tagline;
+        const p3 = document.createElement('p');
+        p3.textContent = price;
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(p1);
+        article.appendChild(p2);
+        article.appendChild(p3);
         return (article);
     }
     return { name, picture, getUserCardDOM }
